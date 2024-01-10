b11451c1e52eb51c26101ac5d6387658ad3f6af1
AndriusVegan <lifecoachandy7@gmail.com>
2024-01-10 20:35:41 +0200

    fetch results and searchPage done, but there is a bug in fetcResults page suntax error

---
+++ app/search/page.tsx
@@ -0,0 +1,27 @@
+import { fetchResults } from "@/lib/fetchResults";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+type Props = {
+  searchParams: SearchParams;
+};
+
+export type SearchParams = {
+  url: URL;
+  group_adults: string;
+  group_children: string;
+  no_rooms: string;
+  checkin: string;
+  checkout: string;
+};
+
+async function SearchPage({ searchParams }: Props) {
+  if (!searchParams.url) return notFound();
+
+  const results = await fetchResults(searchParams);
+  if (!results) return <div>No results...</div>;
+  console.log(results);
+  return <div>SearchPage</div>;
+}
+
+export default SearchPage;
